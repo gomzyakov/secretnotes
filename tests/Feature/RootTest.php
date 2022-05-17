e0db121e7a2c3346748f56aaf4117809909a1f66
gomzyakov <alexander.gomzyakov@gmail.com>
2022-05-17 22:16:11 +0500

    Update RootTest

--- tests/Feature/RootTest.php
+++ tests/Feature/RootTest.php
@@ -5,17 +5,19 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class RootTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
     public function test_the_application_returns_a_successful_response()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_404_page()
+    {
+        $response = $this->get('/xxx');
+
+        $response->assertStatus(404);
     }
 }
