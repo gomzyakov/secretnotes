2974486ed4a06b61cf39823f7a0223346d915dca
gomzyakov <alexander.gomzyakov@gmail.com>
2022-05-21 11:02:19 +0500

    Decrease lines in StoryController.php

--- app/Http/Controllers/StoryController.php
+++ app/Http/Controllers/StoryController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Note;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Crypt;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Contracts\View\Factory as ViewFactory;
+use Illuminate\Contracts\View\View;
 
 /**
  * Контроллер бизнес-кейсов.
@@ -15,71 +13,10 @@ class StoryController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return Response
+     * @return ViewFactory|View
      */
-    public function index()
+    public function index(): ViewFactory|View
     {
         return view('home');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        return view('new-note');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function show($slug)
-    {
-        $note = Note::where('slug', $slug)->first();
-
-        if ($note->expiration_date ?? '' && $note->expiration_date < now()) {
-            $note->delete();
-
-            return view('password-note');
-        }
-
-        $note->password ?? '' === 'none' ? $password = false : $password = true;
-
-        return view('password-note', compact('note', 'password'));
-    }
-
-    public function decrypt($slug)
-    {
-        request()->validate([
-            'decrypt_password' => 'string|max:100',
-        ], [
-            'decrypt_password.string' => 'Le champ est vide ou une erreur est survenue',
-            'decrypt_password.max' => 'Le mot de passe ne peut pas faire plus de 100 caractères',
-        ]);
-
-        $note = Note::where('slug', $slug)->firstOr(
-            function () {
-                return back()->withErrors(['404' => 'Désolé cette note n\'existe pas ou elle a déjà été lue']);
-            }
-        );
-
-        if ($note->password !== 'none') {
-            if (Hash::check(request()->decrypt_password, $note->password)) {
-                $note->text = Crypt::decryptString($note->text);
-            } else {
-                return back()->withErrors(['bad_password' => 'Mot de passe incorrect']);
-            }
-        } else {
-            $note->text = Crypt::decryptString($note->text);
-        }
-
-        $note->delete();
-
-        return view('show-note', compact('note'));
     }
 }
