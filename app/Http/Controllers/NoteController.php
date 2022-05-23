7f4e7eeb9c7d4784ac4ac68341b8f5258ac95a18
gomzyakov <alexander.gomzyakov@gmail.com>
2022-05-23 21:38:55 +0500

    Remove unused methods from NoteController

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -148,38 +148,4 @@ class NoteController extends Controller
 
         return view('show-note', compact('note'));
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param int $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
