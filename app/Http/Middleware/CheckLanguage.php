dbacdae730173f9b9941257f008eadfe72d568fc
gomzyakov <alexander.gomzyakov@gmail.com>
2022-05-30 14:35:14 +0500

    Add controller & middleware

---
+++ app/Http/Middleware/CheckLanguage.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+
+class CheckLanguage
+{
+    const LOCALE_SESSION_KEY = 'locale';
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     *
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+//        if ($request->input('lang') !== 'my-secret-token') {
+//            return redirect('home');
+//        }
+
+        $languages = config('languages');
+
+        if (Session::has(self::LOCALE_SESSION_KEY)
+            && array_key_exists(Session::get(self::LOCALE_SESSION_KEY), $languages)) {
+            App::setLocale(Session::get(self::LOCALE_SESSION_KEY));
+        }
+
+        return $next($request);
+    }
+}
