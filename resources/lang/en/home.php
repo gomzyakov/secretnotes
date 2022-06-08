c6f9a3b97d18cb46a018f26da5a9542b032fa911
Alexander Gomzyakov <alexander.gomzyakov@gmail.com>
2022-06-08 15:51:13 +0500

    Prettify section "How can I use SecretNotes?" on home page (#98)

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -8,5 +8,11 @@ return [
         'prefix' => 'You can create a secret note with ',
         'link'   => 'additional settings',
     ],
-    'create_btn'  => 'Create note',
+    'create_btn' => 'Create note',
+    'cases'      => [
+        'title'        => 'How can I use SecretNotes?',
+        'card_1_title' => 'Share passwords securely at work',
+        'card_2_title' => 'Give out one-time promo codes',
+        'card_3_title' => 'Share secrets with friends',
+    ],
 ];
