662694b6b049b3a8a1e67e0994e361367cc5b211
gomzyakov <alexander.gomzyakov@gmail.com>
2022-09-13 20:36:27 +0500

    Update Bootstrap to v5.2.1

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -15,9 +15,9 @@
     {{-- TODO <meta name="theme-color" content="#7952b3">--}}
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
           rel="stylesheet"
-          integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+          integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT"
           crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
