10c7684310e6fd97b1e073f052f443bb246f8474
gomzyakov <alexander.gomzyakov@gmail.com>
2022-09-20 21:35:28 +0500

    Update Yandex.Metrika counter

--- resources/views/layout/js_scripts.blade.php
+++ resources/views/layout/js_scripts.blade.php
@@ -7,23 +7,18 @@
         crossorigin="anonymous"></script>
 
 <!-- Yandex.Metrika counter -->
-<script type="text/javascript">
-    (function (m, e, t, r, i, k, a) {
-        m[i] = m[i] || function () {
-            (m[i].a = m[i].a || []).push(arguments)
-        };
-        m[i].l = 1 * new Date();
-        k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-    })
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-    ym(88726860, "init", {
-        clickmap: true,
-        trackLinks: true,
-        accurateTrackBounce: true
+    ym(90496068, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
     });
 </script>
-<noscript>
-    <div><img src="https://mc.yandex.ru/watch/88726860" style="position:absolute; left:-9999px;" alt=""/></div>
-</noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/90496068" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
