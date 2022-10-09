2a6e5d2aef077bcb588d8da4e18cb7f393b58679
Alexander Gomzyakov <alexander.gomzyakov@gmail.com>
2022-10-09 12:11:49 +0500

    Show app version on separate URL (#269)

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,9 @@ Route::get('/about', [PageController::class, 'showAboutPage'])->name('about');
 
 Route::get('/lang/{lang}', [LanguageController::class, 'switchLang'])->name('lang.switch');
 
+Route::get('/version', function () {
+    return \Gomzyakov\GitVersion\GitVersion::getVersion();
+});
+
 // TODO To API
 Route::post('/note/{slug}', [NoteController::class, 'decrypt'])->name('note.decrypt');
