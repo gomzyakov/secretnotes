3d36d52e020fb8f369a3b8147207d36acf6d6d2d
Alexander Gomzyakov <alexander.gomzyakov@gmail.com>
2022-10-15 19:13:27 +0500

    v0.37.0 (#284)

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
      | the framework needs to place the application's version in a notification
      | or any other location as required by the application or its packages.
      */
-    'version' => '0.36.0',
+    'version' => '0.37.0',
 
     /*
     |--------------------------------------------------------------------------
