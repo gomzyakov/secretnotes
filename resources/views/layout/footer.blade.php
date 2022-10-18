975522083a5f81dab9e17e8266364a4e33d08beb
Alexander Gomzyakov <alexander.gomzyakov@gmail.com>
2022-10-18 15:57:44 +0500

    Place a link to GitHub (#300)

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -7,28 +7,26 @@
                     <a class="link-dark text-decoration-none footer-logo" href="{{ route('home') }}">
                         Secretic
                     </a>
-                    <div class="text-secondary">
-                        <small>v{{ config('app.version') }}</small>
+                    <small class="text-secondary">v{{ config('app.version') }}</small>
+                    <div class="text-secondary mb-4">
+                        <small>Secret notes that will self-destruct after being read.</small>
                     </div>
-{{--                    <a class="text-decoration-none"--}}
-{{--                       href="https://github.com/gomzyakov"--}}
-{{--                       target="_blank"--}}
-{{--                       style="color: #aaa;">--}}
-{{--                        Alexander Gomzyakov--}}
-{{--                    </a>--}}
                 </div>
                 <div class="col-md-5">
                     <ul class="nav justify-content-md-end">
+
                         <li class="nav-item d-md-none">
                             <a href="{{ route('home') }}" class="nav-link ps-0 pe-2 px-md-2">
                                 Secretic
                             </a>
                         </li>
+
                         <li class="nav-item d-none d-md-block">
                             <a href="{{ route('home') }}" class="nav-link link-secondary ps-0 pe-2 px-md-2">
                                 Home
                             </a>
                         </li>
+
                         <li class="nav-item">
                             <a href="{{ route('page.note.new') }}" class="nav-link link-secondary px-2">
                                 New note
@@ -38,6 +36,12 @@
                         <li class="nav-item">
                             <a href="{{ route('home').'#about' }}" class="nav-link link-secondary px-2">
                                 About
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="https://github.com/gomzyakov/secretic" class="nav-link link-secondary px-2" target="_blank">
+                                <i class="bi bi-github"></i> GitHub
                             </a>
                         </li>
 
