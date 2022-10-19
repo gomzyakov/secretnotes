58d6dbe003871cbe3f589b0501e89adcb7798156
Alexander Gomzyakov <alexander.gomzyakov@gmail.com>
2022-10-19 15:17:40 +0500

    Show "FAQ" block on main page (#304)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\NoteController;
-use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [NoteController::class, 'index'])->name('home');
@@ -9,8 +8,6 @@ Route::get('/', [NoteController::class, 'index'])->name('home');
 Route::get('/new-note', [NoteController::class, 'showCreatePage'])->name('page.note.new');
 Route::get('/note/show-link/{slug}', [NoteController::class, 'showLink'])->name('page.note.show_link');
 Route::get('/note/{slug}', [NoteController::class, 'openLink'])->name('note.open_link');
-
-Route::get('/about', [PageController::class, 'showAboutPage'])->name('about');
 
 // TODO To API
 Route::post('/note/{slug}', [NoteController::class, 'decrypt'])->name('note.decrypt');
