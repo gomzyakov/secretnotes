f310ad8479ea3279e9bec38465ea29bb8410c97d
Alexander Gomzyakov <alexander.gomzyakov@gmail.com>
2022-10-21 18:33:56 +0500

    Raise the PHPStan level to 2 (#312)

---
+++ app/Providers/NotesRepositoryServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use App\Services\NotesRepository;
+use Hashids\Hashids;
+use Illuminate\Support\ServiceProvider;
+
+class NotesRepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Salt for hashing slug.
+     */
+    public const HASHIDS_SALT = ''; // TODO Move to secret envs
+
+    /**
+     * Minima; lenght of slug.
+     */
+    public const HASHIDS_MIN_LENGTH = 5;
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->app->bind(NotesRepository::class, function () {
+            $hashids = new Hashids(
+                self::HASHIDS_SALT,
+                self::HASHIDS_MIN_LENGTH
+            );
+
+            return new NotesRepository($hashids);
+        });
+    }
+}
