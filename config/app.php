54fd72398a5571f3d308b7cddc32f95568841d2f
Alexander Gomzyakov <alexander.gomzyakov@gmail.com>
2022-11-25 10:01:32 +0500

    v1.23.0 (#424)

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
      | the framework needs to place the application's version in a notification
      | or any other location as required by the application or its packages.
      */
-    'version' => '1.22.0',
+    'version' => '1.23.0',
 
     /*
     |--------------------------------------------------------------------------
