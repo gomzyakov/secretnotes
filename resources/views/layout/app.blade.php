52db639f7508201150673b5013a76d311ea38fd6
Alexander Gomzyakov <alexander.gomzyakov@gmail.com>
2022-12-04 17:36:54 +0500

    fix: links to favicons

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,13 +9,11 @@
     <meta name="description" content="Share a confidential note via a web link that will self-destruct after it is read by your intended recipient.">
 
     <!-- Favicons -->
-    <link rel="apple-touch-icon" href="{{ asset('favicon/apple-touch-icon.png') }}" sizes="180x180">
-    <link rel="icon" href="{{ asset('favicon/favicon-32x32.png') }}" sizes="32x32" type="image/png">
-    <link rel="icon" href="{{ asset('favicon/favicon-16x16.png') }}" sizes="16x16" type="image/png">
-    {{-- TODO <link rel="mask-icon" href="/docs/5.1/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">--}}
-    <link rel="icon" href="{{ asset('favicon/favicon.ico') }}">
-    <link rel="shortcut icon" href="{{ asset('assets/logo-full.png') }}" type="image/x-icon">
-    {{-- TODO <meta name="theme-color" content="#7952b3">--}}
+    <link rel="apple-touch-icon" href="{{ asset('assets/logo.png') }}" sizes="180x180">
+    <link rel="icon" href="{{ asset('assets/logo.png') }}" sizes="32x32" type="image/png">
+    <link rel="icon" href="{{ asset('assets/logo.png') }}" sizes="16x16" type="image/png">
+    <link rel="icon" href="{{ asset('assets/logo.png') }}">
+    <link rel="shortcut icon" href="{{ asset('assets/logo.png') }}" type="image/x-icon">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
