9cdd839558f51bcba5f2baf5196ac93ff47c41a2
Joas de Boer <github@joasdb.nl>
2022-12-08 16:30:03 +0100

    Add copy button to decrypted notes page

--- resources/views/note/show.blade.php
+++ resources/views/note/show.blade.php
@@ -17,8 +17,38 @@
                     <textarea class="form-control" id="note-text" rows="8">{{ $note_text }}</textarea>
                 </div>
 
+                <div class="mb-3">
+                    <div class="d-grid">
+                        <button id="copy-button" type="button" class="btn btn-outline-secondary">
+                            Copy note
+                        </button>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
 @endsection
 
+@push('scripts')
+    <script>
+        $(document).ready(function () {
+            $('#copy-button').click(function (e) {
+                e.preventDefault();
+
+                var note_text = document.getElementById("note-text");
+                var copy_btn = document.getElementById("copy-button");
+
+                /* Select the text field */
+                note_text.select();
+                note_text.setSelectionRange(0, 99999); /* For mobile devices */
+
+                /* Copy the text inside the text field */
+                navigator.clipboard.writeText(note_text.value);
+
+                note_text.classList.add("is-valid");
+                copy_btn.innerText = 'Copied!';
+            });
+        });
+    </script>
+@endpush
