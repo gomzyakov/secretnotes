e23fb9c9a021f7bc42d0484e42c07b9c2aacab3f
Alexander Gomzyakov <alexander.gomzyakov@gmail.com>
2023-04-16 18:18:19 +0500

    Update to Laravel 10 (#654)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('cache:prune-stale-tags')->hourly();
     }
 
     /**
